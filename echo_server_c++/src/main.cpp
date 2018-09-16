640982d85f462270ad00707f2609cb3104434d4f
Neo <lin.chaozhi@qq.com>
2018-09-16 23:09:13 +0800

    init should be first

--- echo_server_c++/src/main.cpp
+++ echo_server_c++/src/main.cpp
@@ -6,10 +6,11 @@ int
 main(int argc, char **argv)
 {
     ServerMgr serverMgr;
+    serverMgr.init(argc, argv);
+
     serverMgr.addServer(ServerFactory::createEchoServer());
     serverMgr.addServer(ServerFactory::createTimeServer());
     serverMgr.activeAllServer();
 
-    serverMgr.init(argc, argv);
     serverMgr.start();
 }
